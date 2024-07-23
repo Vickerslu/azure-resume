0c8d4e9b4459bb4c73eeccece5154f4db1728eb1
vickerslu <vickerslu@gmail.com>
2024-07-23 14:44:20 +0100

    Deployed storage and function

--- frontend/main.js
+++ frontend/main.js
@@ -2,7 +2,9 @@ window.addEventListener('DOMContentLoaded', (event) => {
   getVisitCount();
 });
 
-const functionApi = 'http://localhost:7071/api/counter';
+const functionApiLocal = 'http://localhost:7071/api/counter';
+const functionApi = 'https://lukegetresumecounter.azurewebsites.net/api/counter?code=qh6udrYuSMiU5WWKuNiACGm02sYbOF4o5_zPJ8RUFhvfAzFuSX6iNw%3D%3D';
+
 
 const getVisitCount = () => {
   fetch(functionApi)
